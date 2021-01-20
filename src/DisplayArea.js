7b0d42d10e56511e76bc2f33aef65e3714a7c9c6
Steven Laverty <sdlaverty123@gmail.com>
2021-01-20 17:29:27 -0500

    Added top-level components

---
+++ src/DisplayArea.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class DisplayArea extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className='DisplayArea'></div>
+  }
+}
+
+export default DisplayArea;
---
+++ src/MainToolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainToolbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div className='MainToolbar'>
+      Kaleidoscope
+    </div>
+  }
+}
+
+export default MainToolbar;
