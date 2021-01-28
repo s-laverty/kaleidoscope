f5642e4639c9c1aa7b5184f88af55da3f04033c9
Steven Laverty <sdlaverty123@gmail.com>
2021-01-27 22:43:02 -0500

    Refactor passing props

--- src/containers/ToolsMenu.js
+++ src/containers/ToolsMenu.js
@@ -5,27 +5,19 @@ import OptionsTray from './OptionsTray';
 
 class ToolsMenu extends React.Component {
   render() {
-    const MainToolbar_props = {
-      selected_tool: this.props.selected_tool,
-      colors: this.props.colors,
-      selected_color_index: this.props.selected_color_index,
-      color_picker_value: this.props.color_picker_value,
-      handleToolbar: this.props.handleToolbar
-    };
-    const OptionsTray_props = {
-      selected_tool: this.props.selected_tool,
-      color_picker_value: this.props.color_picker_value,
-      will_pick_color: this.props.will_pick_color,
-      handleToolbar: this.props.handleToolbar
-    }
     return <div className='ToolsMenu'>
-      <MainToolbar {...MainToolbar_props}/>
-      {<OptionsTray
+      <MainToolbar
+        selected_tool={this.props.selected_tool}
+        colors={this.props.colors}
+        selected_color_index={this.props.selected_color_index}
+        handleToolbar={this.props.handleToolbar}
+      />
+      <OptionsTray
         selected_tool={this.props.selected_tool}
         color_picker_value={this.props.color_picker_value}
         will_pick_color={this.props.will_pick_color}
         handleToolbar={this.props.handleToolbar}
-      />}
+      />
     </div>
   }
 }
