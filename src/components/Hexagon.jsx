e09cce13661178bb34cb88cbee563dc12ed55fc8
Steven Laverty <sdlaverty123@gmail.com>
2021-04-29 14:01:03 -0400

    Rename Hexagon to jsx

--- src/components/Hexagon.jsx
+++ src/components/Hexagon.jsx
@@ -1,11 +1,7 @@
 import React, { memo } from 'react';
 import './Hexagon.scss';
-import { multiply, transpose, add, subtract, min, max,
-  deepEqual, lup, ceil, lusolve, floor, flatten, inv } from 'mathjs';
-import CustomMap from '../utils/CustomMap';
-import CustomSet from '../utils/CustomSet';
+import { multiply, transpose, add, floor, inv } from 'mathjs';
 import { HexPoint } from '../utils/HexUtils';
-import Point from '../utils/Point';
 
 // Constants
 const SHAPE = transpose([
@@ -25,6 +21,7 @@ export const SPACING_TRANSFORM = multiply(APOTHEM*2 + MARGIN, TRANSFORM);
 export const X_SPACING = SPACING_TRANSFORM[0][0];
 export const Y_SPACING = SPACING_TRANSFORM[1][1];
 const SPACING_INV = inv(SPACING_TRANSFORM);
+/*
 const OUTLINE_VERTICES = multiply(
   RADIUS + BORDER_STROKE/2/Math.cos(Math.PI/6),
   SHAPE
@@ -34,7 +31,7 @@ const OUTLINE_VERTICES_CONCAVE = add(OUTLINE_VERTICES,
     (MARGIN - BORDER_STROKE)/2/Math.cos(Math.PI/6),
     SHAPE.slice(-1).concat(SHAPE.slice(0,-1))
   )
-);
+);*/
 const GRID_VERTICES = multiply(
   RADIUS + MARGIN/2/Math.cos(Math.PI/6),
   SHAPE
@@ -137,122 +134,4 @@ export const HexTileSVG = memo(({hexes, className, ...other}) => {
   );
 });
 
-/*
-class Hexagon_old extends React.PureComponent {
-  static visibleInBox(t,r,b,l) {
-    const x_spacing = SPACING[0][0];
-    const y_spacing = SPACING[1][1];
-    let leftMost = Math.ceil(2*l / x_spacing) - 1; // Measured in half hexes
-    let rightMost = Math.floor(2*r / x_spacing) + 1; // Measured in half hexes
-    let topMost = Math.ceil((t + RADIUS/2) / y_spacing) - 1; // Measured in rows
-    let bottomMost = Math.floor((b - RADIUS/2) / y_spacing) + 1; // Measured in rows
-    let ldiff = leftMost - topMost;
-    let rdiff = rightMost - topMost;
-    return {
-      t: topMost,
-      b: bottomMost,
-      l: Math.ceil(ldiff/2),
-      r: Math.floor(rdiff/2),
-      lskew: Math.abs(ldiff) % 2,
-      rskew: Math.abs(rdiff+1) % 2
-    };
-  }
-
-  static *getTilesInWindow(window, tessellation) {
-    tessellation = transpose(tessellation);
-    const tr_lup = lup(multiply(SPACING, tessellation));
-    let min_point = [Infinity,Infinity];
-    let max_point = [-Infinity,-Infinity];
-    [window[0], window[1], [window[0][0],window[1][1]], [window[1][0],window[0][1]]]
-    .forEach(vertex => {
-      const sol = flatten(lusolve(tr_lup, vertex)).toArray();
-      min_point = min([floor(sol), min_point], 0);
-      max_point = max([ceil(sol), max_point], 0);
-    });
-    for (let i = min_point[0]; i <= max_point[0]; ++i)
-      for (let j = min_point[1]; j <= max_point[1]; ++j)
-        yield new Point(...multiply(tessellation, [i,j]));
-  }
-
-  static Outline = React.memo(props => {
-    const {tile} = props;
-    const points = [];
-    const perimeter = tile.perimeter();
-    perimeter.reduce((prev_point, [point, i]) => {
-      const center = multiply(SPACING, point);
-      if (!deepEqual(point, prev_point))
-        points.push(add(center, OUTLINE_VERTICES_CONCAVE[(i+5)%6]));
-      points.push(add(center, OUTLINE_VERTICES[i]));
-      return point;
-    }, perimeter[perimeter.length-1][0]);
-    return (
-      <svg className='Hexagon_Outline'>
-        <polygon points={points.join(' ')} fill='none'
-        strokeWidth={BORDER_STROKE} stroke={BORDER_COLOR}/>
-      </svg>
-    );
-  });
-
-  static TileRender = React.memo(props => {
-    const {
-      tile,
-      outline=false,
-      color_override,
-      x, y,
-      style
-    } = props;
-    const hexes = [];
-
-    for (let [point, {color='lightgray'}] of tile) {
-      if (color_override) color = color_override;
-      const center = multiply(SPACING, point);
-      hexes.push(
-        <polygon key={point}
-        points={VERTICES.map(vertex => add(center, vertex)).join(' ')}
-        fill={color} stroke='none'/>
-      );
-    }
-    const derived_style = {...style};
-    if (x || y) {
-      derived_style.transform = `translate(${multiply(SPACING, [x,y]).join('px,')}px`;
-      if (style?.transform) derived_style.transform += style.transform;
-    }
-    return (
-      <svg className='Hexagon_Tile' style={derived_style}>
-        {outline && <Hexagon.Outline tile={tile}/>}
-        {hexes}
-      </svg>
-    );
-  });
-
-  render() {
-    const {
-      className,
-      color,
-      x, y,
-      style,
-      draggable=false,
-      ...other
-    } = this.props;
-    const point = new Point(x,y);
-    for (const [key, val] of Object.entries(other)) {
-      if (key.startsWith('on') && typeof val === 'function')
-        other[key] = e => val(e, point);
-    }
-    const derived_className = ['Hexagon'];
-    if (className) derived_className.push(className);
-    const derived_style = {...style,
-      transform: 'translate(-50%,-50%)' +
-        `translate(${multiply(SPACING, point).join('px,')}px`
-    };
-    if (style?.transform) derived_style.transform += style.transform;
-    if (color) derived_style.backgroundColor = color;
-    return (
-      <div className={derived_className.join(' ')} draggable={draggable}
-      style={derived_style}
-      {...other}/>
-    );
-  }
-}
-*/
 export default memo(Hexagon);
