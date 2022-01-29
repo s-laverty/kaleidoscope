0d276250e60eba92a38695bf9a76c454da37a170
Steven Laverty <sdlaverty123@gmail.com>
2022-01-29 14:45:01 -0500

    Updated testing suite for CustomMap / CustomSet

--- src/utils/collections/CustomSet.test.js
+++ src/utils/collections/CustomSet.test.js
@@ -1,66 +1,98 @@
 import CustomSet from './CustomSet';
 
-describe('Basic usage tests', () => {
-  let set1; let set2; let
-    set3;
+describe('Basic usage tests.', () => {
+  let set1 = /** @type {CustomSet<number>} */ (null);
+  let set2 = /** @type {CustomSet<string>} */ (null);
+  let set3 = /** @type {CustomSet<*>} */ (null);
+
   beforeEach(() => {
     set1 = new CustomSet([0, 1, 1, 2, 3, 5, 8]);
     set2 = new CustomSet(['apple', 'banana', 'kiwifruit']);
     set3 = new CustomSet([true, false, null, undefined, Math.log(-3)]);
   });
-  test('No repeats', () => {
+
+  test('Adding new elements.', () => {
+    expect(set2.size).toBe(3);
+    expect(set2.keys()).toEqual(expect.arrayContaining(['apple', 'banana', 'kiwifruit']));
+    expect(set2.has('papaya')).toBe(false);
+    set2.add('papaya');
+    expect(set2.has('papaya')).toBe(true);
+    expect(set2.size).toBe(4);
+    set2.add('lime');
+    expect(set2.size).toBe(5);
+    expect(set2.entries()).toEqual(expect.arrayContaining([
+      ['apple', 'apple'], ['banana', 'banana'], ['kiwifruit', 'kiwifruit'], ['papaya', 'papaya'],
+      ['lime', 'lime'],
+    ]));
+  });
+
+  test('Adding repeat elements.', () => {
     expect(set1.size).toBe(6);
-    expect([...set1]).toEqual([0, 1, 2, 3, 5, 8]);
-    expect(set1.add(5).add(3).add(5).size).toBe(6);
+    expect(set1.values()).toEqual(expect.arrayContaining([0, 1, 2, 3, 5, 8]));
+    expect(set1.has(5)).toBe(true);
+    set1.add(5);
+    expect(set1.has(5)).toBe(true);
+    expect(set1.size).toBe(6);
+    expect(set1.values()).toEqual(expect.arrayContaining([0, 1, 2, 3, 5, 8]));
+  });
+
+  test('Chaing add calls.', () => {
+    expect(set1.add(5).add(-8).add(5).size).toBe(7);
+    expect(set1.values()).toEqual(expect.arrayContaining([-8, 0, 1, 2, 3, 5, 8]));
+  });
+
+  test('Deleting elements.', () => {
     expect(set3.size).toBe(5);
-    expect(set3.has(true)).toBe(true);
+    expect(set3.has(undefined)).toBe(true);
     expect(set3.delete(undefined)).toBe(true);
+    expect(set3.size).toBe(4);
     expect(set3.has(undefined)).toBe(false);
     expect(set3.delete(undefined)).toBe(false);
     expect(set3.size).toBe(4);
-    expect(set3.has(Math.sqrt(-49))).toBe(true);
+    expect(set3.has(undefined)).toBe(false);
   });
-  test('copying', () => {
-    expect([...set2]).not.toEqual([...set3]);
+
+  test('Copying sets.', () => {
     set3.clear();
     expect(set3.size).toBe(0);
     set2.forEach((value) => set3.add(value));
-    expect([...set2]).toEqual([...set3]);
+    expect(set3.size).toBe(3);
+    expect(set3.values()).toEqual(expect.arrayContaining(set2.values()));
     const set4 = new CustomSet(set2);
     set2.clear();
     expect(set4.size).toBe(3);
-    set2 = new CustomSet(set1);
-    set2.delete(5);
+    expect(set2.size).toBe(0);
+    expect(set4.values()).toEqual(expect.arrayContaining(set3.values()));
+    const set5 = new CustomSet(set1);
+    set5.delete(5);
     expect(set1.has(5)).toBe(true);
   });
-  test('iterators', () => {
-    expect([...set1]).toEqual([...set1.keys()]);
-    expect([...set1]).toEqual([...set1.values()]);
+
+  test('Iteration with forEach.', () => {
     const values = [];
     const testObj = { someProperty: 'potato' };
     const counter = jest.fn();
     set1.forEach(function testFunc(value, _value2, set) {
       counter();
+      expect(value).toBe(_value2);
       values.push(value);
       expect(this.someProperty).toBe('potato');
       expect(set).toBe(set1);
     }, testObj);
     expect(counter).toHaveBeenCalledTimes(6);
-    expect(values).toEqual([...set1]);
-    [...set1.entries()].forEach((entry, i) => {
-      expect(entry[0]).toBe(values[i]);
-      expect(entry[1]).toBe(values[i]);
-      expect(entry).toHaveLength(2);
-    });
+    expect(values.length).toBe(set1.size);
+    expect(values).toEqual(expect.arrayContaining(set1.values()));
   });
 });
 
-describe('Hash function tests', () => {
-  let set1;
+describe('Hash function tests.', () => {
+  let set1 = /** @type {CustomSet<*>} */ (null);
+
   beforeEach(() => {
     set1 = new CustomSet(null, String);
   });
-  test('Normal functionality', () => {
+
+  test('Basic functionality.', () => {
     set1.add(50);
     expect(set1.has(50)).toBe(true);
     expect(set1.size).toBe(1);
@@ -68,29 +100,43 @@ describe('Hash function tests', () => {
     expect(set1.size).toBe(1);
     set1.add(12).add(54);
     expect(set1.delete(12)).toBe(true);
-    expect([...set1]).toEqual(expect.arrayContaining([50, 54]));
-    const set2 = new CustomSet(set1);
-    expect([...set2]).toEqual([...set1]);
-    const set3 = new CustomSet(set1, String);
-    expect([...set3]).toEqual([...set1]);
+    expect(set1.size).toBe(2);
+    expect(set1.values()).toEqual(expect.arrayContaining([50, 54]));
+    const set2 = new CustomSet(set1, String);
+    expect(set2.size).toBe(2);
+    expect(set1.values()).toEqual(expect.arrayContaining([50, 54]));
   });
-  test('Hash collisions', () => {
+
+  test('Hash collisions.', () => {
     set1.add(50);
     expect(set1.has('50')).toBe(true);
     set1.add(['words', 'on', 'a', 'page']);
     expect(set1.has('words,on,a,page')).toBe(true);
     expect(set1.delete('words,on,a,page')).toBe(true);
-    expect(String([...set1][0])).toBe('50');
+    expect(set1.size).toBe(1);
+    expect(set1.values().map(String)).toEqual(expect.arrayContaining(['50']));
+  });
+
+  test('Hash collisions in new instance.', () => {
+    const set2 = new CustomSet([50, '50']);
+    expect(set2.size).toBe(2);
+    const set3 = new CustomSet(set2, String);
+    expect(set3.size).toBe(1);
+    expect(set3.has(50)).toBe(true);
+    expect(set3.has('50')).toBe(true);
   });
 });
 
 describe('Equal function tests', () => {
   const typeEqual = (obj1, obj2) => typeof obj1 === typeof obj2;
-  let set1;
+
+  let set1 = /** @type {CustomSet<*>} */ (null);
+
   beforeEach(() => {
     set1 = new CustomSet(null, String, typeEqual);
   });
-  test('Normal functionality', () => {
+
+  test('Basic functionality.', () => {
     set1.add(50);
     expect(set1.has(50)).toBe(true);
     expect(set1.size).toBe(1);
@@ -98,28 +144,35 @@ describe('Equal function tests', () => {
     expect(set1.size).toBe(1);
     set1.add(12).add(54);
     expect(set1.delete(12)).toBe(true);
-    expect([...set1]).toEqual(expect.arrayContaining([50, 54]));
-    const set2 = new CustomSet(set1);
-    expect([...set2]).toEqual([...set1]);
-    const set3 = new CustomSet(set1, String, typeEqual);
-    expect([...set3]).toEqual([...set1]);
+    expect(set1.size).toBe(2);
+    expect(set1.values()).toEqual(expect.arrayContaining([50, 54]));
+    const set2 = new CustomSet(set1, String, typeEqual);
+    expect(set2.size).toBe(2);
+    expect(set1.values()).toEqual(expect.arrayContaining([50, 54]));
   });
-  test('Hash collisions', () => {
+
+  test('Hash collisions.', () => {
     set1.add(50);
     expect(set1.has('50')).toBe(false);
     set1.add('50');
-    expect(set1.has('50')).toBe(true);
     expect(set1.size).toBe(2);
-    const set2 = new CustomSet(set1, String);
-    expect(set2.size).toBe(1);
-    const set3 = new CustomSet(set1, String, typeEqual);
-    expect(set3.delete('50')).toBe(true);
-    expect(set1.has('50')).toBe(true);
-    expect(set3.delete(50)).toBe(true);
-    expect(set3.size).toBe(0);
     set1.add(['words', 'on', 'a', 'page']);
     expect(set1.has('words,on,a,page')).toBe(false);
     expect(set1.delete('words,on,a,page')).toBe(false);
-    expect(set1.values()).toContainEqual(['words', 'on', 'a', 'page']);
+    expect(set1.size).toBe(3);
+    expect(set1.values()).toEqual(expect.arrayContaining([
+      50, '50', ['words', 'on', 'a', 'page'],
+    ]));
+    expect(set1.has(['words', 'on', 'a', 'page'])).toBe(true);
+    expect(set1.has(['words,on,a,page'])).toBe(true);
+  });
+
+  test('Hash collisions in new instance.', () => {
+    const set2 = new CustomSet([50, '50']);
+    expect(set2.size).toBe(2);
+    const set3 = new CustomSet(set2, String, typeEqual);
+    expect(set3.size).toBe(2);
+    expect(set3.has(50)).toBe(true);
+    expect(set3.has('50')).toBe(true);
   });
 });
