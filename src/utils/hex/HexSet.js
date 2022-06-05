17f48a9d976a926e319003e2ceeae9fa064e6bcc
Steven Laverty <sdlaverty123@gmail.com>
2022-06-05 11:55:33 -0400

    Added HexSet data structure

---
+++ src/utils/hex/HexSet.js
@@ -0,0 +1,344 @@
+import PointSet from '../point/PointSet';
+import PointMap from '../point/PointMap';
+import HexComponent from './HexComponent';
+/** @typedef {import('./HexPoint').default} HexPoint */
+/** @typedef {import('./HexPoint').HexPointEdge} HexPointEdge */
+/** @typedef {import('./HexPoint').Edges} Edges */
+
+/**
+ * HexSet is an extension of PointSet which keeps track of all connected components of hexes within
+ * the set.
+ * @extends {PointSet<HexPoint>}
+ */
+export default class HexSet extends PointSet {
+  /**
+   * An array of hex point & edge tuples.
+   * @typedef {HexPointEdge[]} BorderTrace
+   */
+
+  /**
+   * A mapping of points on the edges of components to edges indicating the exterior of the
+   * component.
+   * @type {PointMap<Edges, HexPoint>}
+   */
+  #edges;
+
+  /**
+   * A mapping of points adjacent to components to edges indicating the exterior of the component.
+   * @type {PointMap<Edges, HexPoint>}
+   */
+  #adjacent;
+
+  /**
+   * A set of all components in this HexSet.
+   * @type {Set<HexComponent>}
+   */
+  #components = new Set();
+
+  /**
+   * A mapping of hex points to the components that contain them.
+   * @type {PointMap<HexComponent, HexPoint>}
+   */
+  #componentPoints = new PointMap();
+
+  /**
+   * Creates a HexSet.
+   * @param {HexPoint[] | Set<HexPoint> | HexSet} [entries] - An optional collection of HexPoints
+   * used to initialize the set.
+   */
+  constructor(entries) {
+    if (entries instanceof HexSet) {
+      // Clone the provided instance of HexSet.
+      super(entries);
+      this.#edges = new PointMap(entries.#edges);
+      this.#adjacent = new PointMap(entries.#adjacent);
+
+      entries.#components.forEach((component) => {
+        const newComponent = new HexComponent(component);
+        this.#components.add(newComponent);
+        newComponent.forEach((point) => this.#componentPoints.set(point, newComponent));
+      });
+    } else {
+      // Initialize a new HexSet.
+      super();
+
+      this.#edges = new PointMap();
+      this.#adjacent = new PointMap();
+
+      if (entries instanceof Array) entries.forEach((point) => this.add(point));
+      else entries?.forEach((point) => this.add(point));
+    }
+  }
+
+  /**
+   * Traces an edge of a component in a complete cycle (left to right from inside the component) and
+   * returns an array of tuples indicating each step.
+   * @param {HexPointEdge} start - A HexPoint & edge number tuple indicating the start of the trace
+   * (will be yielded first).
+   * @returns {BorderTrace} An array of HexPoint & edge number tuples forming a complete traversal
+   * of a component border.
+   */
+  #traceBorder(start) {
+    let [point, i] = start;
+    let edges = this.#edges.get(point);
+    const trace = /** @type {BorderTrace} */ ([]);
+
+    // Follow along the border edge.
+    do {
+      trace.push([point, i]);
+      i = (i + 1) % 6;
+
+      if (~edges & (1 << i)) {
+        point = point.step(i);
+        edges = this.#edges.get(point);
+        i = (i + 4) % 6;
+      }
+    }
+    while (!point.equals(start[0]) || i !== start[1]);
+
+    return trace;
+  }
+
+  /**
+   * Adds a point to the HexSet.
+   * @param {HexPoint} point - The point to associate with the value.
+   * @returns {HexSet} The HexSet.
+   */
+  add(point) {
+    if (!this.has(point)) {
+      // Update the surrounding edge and adjacent hexes.
+      this.#adjacent.delete(point);
+      let edges = 0;
+      let component = /** @type {HexComponent} */ (null);
+
+      point.adjacent.forEach((adjPoint, i) => {
+        const edge = 1 << (i + 3) % 6;
+        if (this.has(adjPoint)) {
+          const adjEdges = this.#edges.get(adjPoint) & ~edge;
+
+          if (!adjEdges) this.#edges.delete(adjPoint);
+          else this.#edges.set(adjPoint, adjEdges);
+
+          // Merge all adjacent components into one.
+          const adjComponent = this.#componentPoints.get(adjPoint);
+          if (!component) {
+            component = adjComponent;
+            component.add(point);
+          } else if (adjComponent !== component) {
+            component.merge(adjComponent, point, adjPoint);
+            this.#components.delete(adjComponent);
+            adjComponent.forEach((otherPoint) => this.#componentPoints.set(otherPoint, component));
+          }
+        } else {
+          edges |= 1 << i;
+          this.#adjacent.set(adjPoint, (this.#adjacent.get(adjPoint) ?? 0) | edge);
+        }
+      });
+
+      // Add the hex's component reference.
+      if (edges) {
+        this.#edges.set(point, edges);
+        if (!component) {
+          component = new HexComponent(point);
+          this.#components.add(component);
+        }
+      }
+      this.#componentPoints.set(point, component);
+    }
+
+    super.add(point);
+
+    return this;
+  }
+
+  /** Clears all points in the set. */
+  clear() {
+    super.clear();
+
+    // Clear all components.
+    this.#edges.clear();
+    this.#adjacent.clear();
+    this.#components.clear();
+    this.#componentPoints.clear();
+  }
+
+  /**
+   * Attempts to delete a given point.
+   * @param {HexPoint} point - The point to delete.
+   * @returns {boolean} Whether the point was deleted.
+   */
+  delete(point) {
+    if (super.delete(point)) {
+      // Update the surrounding edge and adjacent hexes.
+      this.#edges.delete(point);
+      let edges = 0;
+
+      point.adjacent.forEach((adjPoint, i) => {
+        const edge = 1 << (i + 3) % 6;
+
+        if (!this.has(adjPoint)) {
+          const adjEdges = this.#adjacent.get(adjPoint) & ~edge;
+
+          if (!adjEdges) this.#adjacent.delete(adjPoint);
+          else this.#adjacent.set(adjPoint, adjEdges);
+        } else {
+          edges |= 1 << i;
+          this.#edges.set(adjPoint, (this.#edges.get(adjPoint) ?? 0) | edge);
+        }
+      });
+
+      // Update the component that contained this hex.
+      const component = this.#componentPoints.get(point);
+      this.#componentPoints.delete(point);
+
+      if (!edges) this.#components.delete(component);
+      else {
+        this.#adjacent.set(point, edges);
+
+        // If deleting this hex splits its component, add the split off components to this set.
+        component.delete(point).forEach((newComponent) => {
+          this.#components.add(newComponent);
+          newComponent.forEach((otherPoint) => this.#componentPoints.set(otherPoint, newComponent));
+        });
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Returns an array of all HexPoints making up the edges of components.
+   * @returns {HexPoint[]} An array of edge HexPoints.
+   */
+  edges() { return this.#edges.keys(); }
+
+  /**
+   * Returns an array of adjacent HexPoints to those in the HexSet.
+   * @returns {HexPoint[]} An array of adjacent HexPoints.
+   */
+  adjacent() { return this.#adjacent.keys(); }
+
+  /**
+   * Checks whether another HexSet overlaps this one.
+   * @param {HexSet} other - The HexSet to compare.
+   * @returns {boolean} Whether the other HexSet overlaps this one.
+   */
+  overlaps(other) { return this.keys().some(other.has, other); }
+
+  /**
+   * Checks whether another HexSet is adjacent to this one.
+   * @param {HexSet} other - The HexSet to compare.
+   * @returns {boolean} Whether the other HexSet is adjacent to this one.
+   */
+  adjacentTo(other) { return this.#adjacent.keys().some(other.has, other); }
+
+  /**
+   * Merges another HexSet into this HexSet.
+   * @param {HexSet} src - The HexSet to merge into this one.
+   * @returns {HexSet} This Hexmap.
+   */
+  merge(src) {
+    // Validate arguments.
+    if (this.overlaps(src)) throw new Error('Provided source HexSet must not overlap this HexSet.');
+
+    // Merge source component one hex at a time.
+    src.forEach((point) => this.add(point));
+    return this;
+  }
+
+  /**
+   * Returns whether this HexSet has just one connected component.
+   * @returns {boolean} Whether this HexSet has just one connected component.
+   */
+  isConnected() { return this.#components.size === 1; }
+
+  /**
+   * Returns an array of point-edge tuples tracing the perimeter, clockwise.
+   * @returns {BorderTrace} An array of point-edge tuples.
+   */
+  perimeter() {
+    // Validate internal state.
+    if (!this.isConnected()) throw new Error('HexSet is not connected.');
+
+    // Get perimeter trace.
+    const component = /** @type {HexComponent} */ (this.#components.values().next().value);
+    const [point, edges] = component.perimeter()[0];
+    let i = 0;
+    while (~edges & (1 << i)) ++i;
+    return this.#traceBorder([point.step(i), (i + 3) % 6]);
+  }
+
+  /**
+   * Returns an array of arrays of point-edge tuples tracing the perimeter of a hole for each hole,
+   * counter-clockwise.
+   * @returns {BorderTrace[]} An array of arrays of point-edge tuples.
+   */
+  holes() {
+    // Validate internal state.
+    if (!this.isConnected()) throw new Error('HexSet is not connected.');
+
+    // Get hole traces.
+    const component = /** @type {HexComponent} */ (this.#components.values().next().value);
+    return component.holes().map((hole) => {
+      const [point, edges] = hole[0];
+      let i = 0;
+
+      while (~edges & (1 << i)) ++i;
+
+      return this.#traceBorder([point.step(i), (i + 3) % 6]);
+    });
+  }
+
+  /**
+   * Creates and returns a translated version of this HexSet.
+   * @param {HexPoint} translation - The translation to apply to this HexSet.
+   * @returns {HexSet} The translated HexSet.
+   */
+  translate(translation) {
+    // Initialize a new HexSet and translate and copy this one.
+    const newSet = /** @type {HexSet} */ (new this.constructor());
+    this.forEach((point) => super.add.call(newSet, point.add(translation)));
+    this.#edges.forEach((value, point) => newSet.#edges.set(point.add(translation), value));
+    this.#adjacent.forEach((value, point) => newSet.#adjacent.set(point.add(translation), value));
+
+    this.#components.forEach((component) => {
+      const newComponent = component.translate(translation);
+      newSet.#components.add(newComponent);
+      newComponent.forEach((point) => newSet.#componentPoints.set(point, newComponent));
+    });
+
+    return newSet;
+  }
+
+  /**
+   * Creates and returns a new HexSet containing only a single component
+   * specified by a chosen starting point within the extracted component.
+   * @param {HexPoint} start - The point specifying which component to extract.
+   * @returns {HexSet} A new HexSet containing only the chosen component.
+   */
+  getComponent(start) {
+    // Validate arguments.
+    if (!this.has(start)) throw new Error('Must provide a starting point in this HexSet.');
+
+    // Initialize new HexSet and copy the chosen component.
+    const newSet = /** @type {HexSet} */ (new this.constructor());
+    const component = this.#componentPoints.get(start);
+    const newComponent = new HexComponent(component);
+    newSet.#components.add(newComponent);
+
+    component.forEach((point) => {
+      super.add.call(newSet, point);
+      newSet.#componentPoints.set(point, newComponent);
+
+      if (this.#edges.has(point)) newSet.#edges.set(point, this.#edges.get(point));
+    });
+
+    component.borders().forEach((border) => (
+      border.forEach(([point, edges]) => newSet.#adjacent.set(point, edges))
+    ));
+
+    return newSet;
+  }
+}
---
+++ src/utils/hex/HexSet.test.js
@@ -0,0 +1,283 @@
+import HexSet from './HexSet';
+import HexPoint from './HexPoint';
+
+const points = new Map(/** @type {[number, number][]} */([
+  [0, -2],
+  [1, -2],
+  [2, -2],
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [2, -1],
+  [-2, 0],
+  [-1, 0],
+  [0, 0],
+  [1, 0],
+  [2, 0],
+  [-2, 1],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [-2, 2],
+  [-1, 2],
+  [0, 2],
+]).map((point) => [String(point), new HexPoint(...point)]));
+
+let set1 = /** @type {HexSet} */ (null);
+let set2 = /** @type {HexSet} */ (null);
+let set3 = /** @type {HexSet} */ (null);
+
+beforeEach(() => {
+  set1 = new HexSet([
+    points.get('0,0'), points.get('-1,-1'), points.get('0,-1'),
+    points.get('1,-2'),
+  ]);
+  set2 = new HexSet([
+    points.get('2,0'), points.get('2,-1'), points.get('-1,1'),
+    points.get('2,-2'), points.get('-2,2'),
+  ]);
+  set3 = new HexSet([
+    points.get('0,2'), points.get('1,0'), points.get('-1,1'),
+  ]);
+});
+
+describe('Basic usage tests.', () => {
+  test('Adding elements.', () => {
+    expect(set1.size).toBe(4);
+    expect(set1.keys()).toEqual(expect.arrayContaining([
+      points.get('-1,-1'), points.get('1,-2'), points.get('0,0'), points.get('0,-1'),
+    ]));
+    expect(set1.values()).toEqual(expect.arrayContaining([
+      points.get('-1,-1'), points.get('1,-2'), points.get('0,0'), points.get('0,-1'),
+    ]));
+    expect(set1.entries()).toEqual(expect.arrayContaining([
+      [points.get('-1,-1'), points.get('-1,-1')], [points.get('1,-2'), points.get('1,-2')],
+      [points.get('0,0'), points.get('0,0')], [points.get('0,-1'), points.get('0,-1')],
+    ]));
+    expect(set1.has(points.get('2,0'))).toBe(false);
+    set1.add(points.get('2,0'));
+    expect(set1.has(points.get('2,0'))).toBe(true);
+    set1.add(points.get('2,-1'));
+    expect(set1.size).toBe(6);
+    expect(set1.values()).toEqual(expect.arrayContaining([
+      points.get('0,0'), points.get('-1,-1'), points.get('0,-1'),
+      points.get('1,-2'), points.get('2,0'), points.get('2,-1'),
+    ]));
+  });
+
+  test('Re-adding elements.', () => {
+    expect(set2.size).toBe(5);
+    expect(set2.values()).toEqual(expect.arrayContaining([
+      points.get('2,0'), points.get('2,-1'), points.get('-1,1'),
+      points.get('2,-2'), points.get('-2,2'),
+    ]));
+    expect(set2.has(points.get('2,-1'))).toBe(true);
+    set2.add(points.get('2,-1'));
+    expect(set2.size).toBe(5);
+    expect(set2.has(points.get('2,-1'))).toBe(true);
+    expect(set2.values()).toEqual(expect.arrayContaining([
+      points.get('2,0'), points.get('2,-1'), points.get('-1,1'),
+      points.get('2,-2'), points.get('-2,2'),
+    ]));
+  });
+
+  test('Chaing add calls.', () => {
+    set1.add(points.get('-1,-1'))
+      .add(points.get('0,0'))
+      .add(points.get('-1,2'))
+      .add(points.get('-1,-1'));
+    expect(set1.size).toBe(5);
+    expect(set1.values()).toEqual(expect.arrayContaining([
+      points.get('0,0'), points.get('-1,-1'), points.get('0,-1'),
+      points.get('1,-2'), points.get('-1,2'),
+    ]));
+  });
+
+  test('Deleting elements.', () => {
+    expect(set3.size).toBe(3);
+    expect(set3.has(points.get('0,2'))).toBe(true);
+    expect(set3.delete(points.get('0,2'))).toBe(true);
+    expect(set3.size).toBe(2);
+    expect(set3.has(points.get('0,2'))).toBe(false);
+    expect(set3.delete(points.get('0,2'))).toBe(false);
+    expect(set3.size).toBe(2);
+    expect(set3.has(points.get('0,2'))).toBe(false);
+  });
+
+  test('Copying sets.', () => {
+    set3.clear();
+    expect(set3.size).toBe(0);
+    set2.forEach((point) => set3.add(point));
+    expect(set3.size).toBe(5);
+    expect(set3.entries()).toEqual(expect.arrayContaining(set2.entries()));
+    const set4 = new HexSet(set2);
+    set2.clear();
+    expect(set4.size).toBe(5);
+    expect(set2.size).toBe(0);
+    expect(set4.entries()).toEqual(expect.arrayContaining(set3.entries()));
+    const set5 = new HexSet(set1);
+    set5.delete(points.get('0,-1'));
+    expect(set1.has(points.get('0,-1'))).toBe(true);
+  });
+
+  test('Iteration with forEach.', () => {
+    const entries = [];
+    const testObj = { someProperty: 'potato' };
+    const counter = jest.fn();
+    set1.forEach(function testFunc(point1, point2, set) {
+      counter();
+      entries.push([point1, point2]);
+      expect(this.someProperty).toBe('potato');
+      expect(set).toBe(set1);
+    }, testObj);
+    expect(counter).toHaveBeenCalledTimes(4);
+    expect(entries).toHaveLength(set1.size);
+    expect(entries).toEqual(expect.arrayContaining(set1.entries()));
+  });
+});
+
+describe('Component tests.', () => {
+  test('Breaking apart a component.', () => {
+    expect(set1.isConnected()).toBe(true);
+    expect(set1.holes()).toHaveLength(0);
+    expect(set1.perimeter()).toHaveLength(18);
+    const set4 = set1.translate(points.get('0,1'));
+    const perimeter = set4.perimeter();
+    expect(perimeter).toHaveLength(18);
+    const startIndex = perimeter.findIndex(
+      ([point, edge]) => point.equals(points.get('0,0')) && edge === 0,
+    );
+    expect(startIndex).not.toBe(-1);
+    expect(perimeter.slice(startIndex).concat(perimeter.slice(0, startIndex))).toEqual([
+      [points.get('0,0'), 0], [points.get('0,1'), 5], [points.get('0,1'), 0],
+      [points.get('0,1'), 1], [points.get('0,1'), 2], [points.get('0,1'), 3],
+      [points.get('0,0'), 2], [points.get('-1,0'), 1], [points.get('-1,0'), 2],
+      [points.get('-1,0'), 3], [points.get('-1,0'), 4], [points.get('-1,0'), 5],
+      [points.get('0,0'), 4], [points.get('1,-1'), 3], [points.get('1,-1'), 4],
+      [points.get('1,-1'), 5], [points.get('1,-1'), 0], [points.get('1,-1'), 1],
+    ]);
+    set1.delete(points.get('0,-1'));
+    expect(() => set1.perimeter()).toThrowError();
+    expect(set1.isConnected()).toBe(false);
+    expect(set4.isConnected()).toBe(true);
+  });
+
+  test('Connecting multiple components.', () => {
+    expect(set3.isConnected()).toBe(false);
+    expect(() => set3.perimeter()).toThrowError();
+    let edges = set3.edges();
+    expect(edges).toHaveLength(3);
+    expect(edges).toEqual(expect.arrayContaining([
+      points.get('0,2'), points.get('1,0'), points.get('-1,1'),
+    ]));
+    let adjacent = set3.adjacent();
+    expect(adjacent).toHaveLength(13);
+    expect(adjacent).toContainEqual(points.get('0,1'));
+    expect(adjacent).not.toContainEqual([points.get('0,2')]);
+    expect(adjacent).not.toContainEqual(points.get('1,0'));
+    expect(adjacent).not.toContainEqual(points.get('-1,1'));
+    set3.add(points.get('0,1'));
+    expect(set3.isConnected()).toBe(true);
+    expect(set3.perimeter()).toHaveLength(18);
+    edges = set3.edges();
+    expect(edges).toHaveLength(4);
+    expect(edges).toEqual(expect.arrayContaining([
+      points.get('0,2'), points.get('1,0'), points.get('-1,1'), points.get('0,1'),
+    ]));
+    adjacent = set3.adjacent();
+    expect(adjacent).toHaveLength(12);
+    expect(adjacent).not.toContainEqual(points.get('0,1'));
+  });
+
+  test('Overlapping.', () => {
+    expect(set1.overlaps(set2)).toBe(false);
+    expect(set2.overlaps(set1)).toBe(false);
+    expect(set3.overlaps(set2)).toBe(true);
+    expect(set2.overlaps(set3)).toBe(true);
+  });
+
+  test('Adjacency', () => {
+    expect(set1.adjacentTo(set2)).toBe(true);
+    expect(set2.adjacentTo(set1)).toBe(true);
+    expect(set3.adjacentTo(set2)).toBe(true);
+    expect(set2.adjacentTo(set3)).toBe(true);
+    set1.delete(points.get('0,0'));
+    expect(set1.adjacentTo(set3)).toBe(false);
+    expect(set3.adjacentTo(set1)).toBe(false);
+  });
+
+  test('Getting a component (invalid).', () => {
+    expect(() => set2.getComponent(points.get('1,-1'))).toThrowError();
+    expect(() => set1.getComponent(points.get('-2,0'))).toThrowError();
+  });
+
+  test('Getting a component (valid)', () => {
+    expect(set2.isConnected()).toBe(false);
+    const set4 = set2.getComponent(points.get('-2,2'));
+    expect(set4.size).toBe(2);
+    expect(set4.isConnected()).toBe(true);
+    expect(set4.values()).toEqual(expect.arrayContaining([
+      points.get('-1,1'), points.get('-2,2'),
+    ]));
+    expect(set4.perimeter()).toHaveLength(10);
+    const set5 = set2.getComponent(points.get('2,-1'));
+    expect(set5.size).toBe(3);
+    expect(set5.isConnected()).toBe(true);
+    expect(set5.values()).toEqual(expect.arrayContaining([
+      points.get('2,0'), points.get('2,-1'), points.get('2,-2'),
+    ]));
+    expect(set5.perimeter()).toHaveLength(14);
+  });
+
+  test('Merging components (invalid).', () => {
+    expect(() => set2.merge(set3)).toThrowError();
+    expect(() => set3.merge(set2)).toThrowError();
+  });
+
+  test('Merging components with holes (valid).', () => {
+    const set4 = set3.translate(points.get('0,-1'));
+
+    // Merge set 2 and set 1, add (1,0) to make a hole.
+    set2.merge(set1).add(points.get('1,0'));
+    const holes = set2.holes();
+    expect(holes).toHaveLength(1);
+    const [hole] = holes;
+    expect(hole).toHaveLength(6);
+    const startIndex = hole.findIndex(
+      ([point, edge]) => point.equals(points.get('0,-1')) && edge === 0,
+    );
+    expect(startIndex).not.toBe(-1);
+    expect(hole.slice(startIndex).concat(hole.slice(0, startIndex))).toEqual([
+      [points.get('0,-1'), 0], [points.get('0,0'), 5], [points.get('1,0'), 4],
+      [points.get('2,-1'), 3], [points.get('2,-2'), 2], [points.get('1,-2'), 1],
+    ]);
+    expect(set2.isConnected()).toBe(true);
+    expect(set2.edges()).toHaveLength(10);
+    expect(set2.adjacent()).toHaveLength(20);
+    expect(set2.perimeter()).toHaveLength(32);
+
+    // Merge set 4 and the previous merge. Should fill in the hole.
+    expect(set4.merge(set2)).toBe(set4);
+    expect(set4.isConnected()).toBe(true);
+    expect(set4.holes()).toHaveLength(0);
+    expect(set4.perimeter()).toHaveLength(30);
+    let edges = set4.edges();
+    expect(edges).toHaveLength(11);
+    expect(edges).not.toContainEqual(points.get('0,0'));
+    expect(edges).not.toContainEqual(points.get('1,-1'));
+
+    // Create a hole, should make (0,0) an interior edge.
+    set4.delete(points.get('1,-1'));
+    edges = set4.edges();
+    expect(edges).toHaveLength(12);
+    expect(edges).toContainEqual(points.get('0,0'));
+    expect(set4.adjacent()).toHaveLength(19);
+
+    // Delete an entry to split the component.
+    set4.delete(points.get('-1,1'));
+    expect(set4.isConnected()).toBe(false);
+    expect(() => set4.holes()).toThrowError();
+    expect(set4.getComponent(points.get('0,0')).holes()).toHaveLength(1);
+    expect(set4.getComponent(points.get('-2,2')).holes()).toHaveLength(0);
+  });
+});
